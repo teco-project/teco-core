2d683d79b873e572518e10f3fc8d599dbc4f437a
stevapple <stevapple@icloud.com>
2022-12-04 05:41:43 +0800

    Implements `TCError` types

--- Package.swift
+++ Package.swift
@@ -8,6 +8,9 @@ let package = Package(
     platforms: [.macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
+       .library(
+           name: "TecoCore",
+           targets: ["TecoCore"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,6 +21,12 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "TecoCore",
+            dependencies: [
+                .product(name: "NIOHTTP1", package: "swift-nio"),
+            ]
+        ),
         .target(
             name: "TecoSigner",
             dependencies: [
