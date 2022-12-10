08f4833f0faf3a8f5bf67f5e8c875ebfd474c202
stevapple <stevapple@icloud.com>
2022-12-11 07:01:01 +0800

    Adds support for older and broader OSes

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "teco-core",
-    platforms: [.macOS(.v11)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
        .library(
