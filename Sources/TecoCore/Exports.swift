8bb53a2c8e77cf604838d5ec7473ba4125a4605a
stevapple <stevapple@icloud.com>
2022-12-10 21:11:24 +0800

    Exports common data types

---
+++ Sources/TecoCore/Exports.swift
@@ -0,0 +1,28 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Teco open source project.
+//
+// Copyright (c) 2022 the Teco project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Teco project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+@_exported import protocol TecoSigner.Credential
+@_exported import struct TecoSigner.StaticCredential
+
+@_exported import struct Logging.Logger
+
+@_exported import struct NIOCore.ByteBuffer
+@_exported import struct NIOCore.ByteBufferAllocator
+@_exported import protocol NIOCore.EventLoop
+@_exported import class NIOCore.EventLoopFuture
+@_exported import protocol NIOCore.EventLoopGroup
+@_exported import struct NIOCore.TimeAmount
+
+@_exported import struct NIOHTTP1.HTTPHeaders
+@_exported import enum NIOHTTP1.HTTPMethod
