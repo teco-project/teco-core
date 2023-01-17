e3b76aab15387a9e218e128fe0b3aa6247e7628f
stevapple <stevapple@icloud.com>
2023-01-17 14:07:18 +0800

    Implements `TCRegion.isAccessible(from:)` helper

--- Sources/TecoCore/Common/TCRegion.swift
+++ Sources/TecoCore/Common/TCRegion.swift
@@ -241,10 +241,27 @@ public struct TCRegion: Equatable, Sendable {
     public static func other(_ id: String, kind: Kind = .internal) -> TCRegion {
         TCRegion(id: id, kind: kind)
     }
+
+    public static func == (lhs: TCRegion, rhs: TCRegion) -> Bool {
+        lhs.rawValue == rhs.rawValue
+    }
 }
 
 extension TCRegion: CustomStringConvertible {
     public var description: String {
         return self.rawValue
+    }
+}
+
+extension TCRegion {
+    /// Whether a region is accessible from another.
+    public func isAccessible(from region: TCRegion) -> Bool {
+        if self == region {
+            return true
+        }
+        guard self.kind == region.kind else {
+            return false
+        }
+        return self.kind == .global || self.kind == .financial
     }
 }
