c8dc0e9345e70f622f7896012c20dacff1692b0e
YR Chen <stevapple@icloud.com>
2023-10-06 09:51:58 +0800

    Introduces globally shared `TCClient`

---
+++ Sources/TecoCore/Utils/Singleton.swift
@@ -0,0 +1,26 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Teco open source project
+//
+// Copyright (c) 2023 the Teco project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+extension TCClient {
+    /// A globally shared, singleton ``TCClient``.
+    ///
+    /// This shared client instance uses a singleton `EventLoopGroup` and cannot be shut down.
+    public static var shared: TCClient {
+        globallySharedTCClient
+    }
+}
+
+private let globallySharedTCClient: TCClient = {
+    let client = TCClient(logger: TCClient.loggingDisabled)
+    return client
+}()
